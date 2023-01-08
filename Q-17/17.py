3adc7b31a5ba83a26265114d785c66f6774549fb
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2023-01-08 21:52:12 +0330

    Update Q-17 Question and add README file

--- Q-17/17.py
+++ Q-17/17.py
@@ -1,5 +1,8 @@
-count = 0
-for number in range(499, 2, -2):
+# Circular Primes
+count = 2
+# Count is one because we do not check '2' and '5' Numbers
+# Note That we should just check odd numbers so dont forget to start with odd numbers because count of range is -2!
+for number in range((10**6)-1, 2, -2):
     flag = True
     for i in range(2, number // 2 + 1):
         if number % i == 0:
@@ -7,7 +10,7 @@ for number in range(499, 2, -2):
             break
     if not flag:
         continue
-    for i in range(5, -1, -1):
+    for i in range(6, -1, -1):
         if number > (10**i):
             p = i
             break
@@ -19,8 +22,6 @@ for number in range(499, 2, -2):
         if r == 5 or (r % 2 == 0):
             flag = False
             break
-        elif number == 0:
-            break
         c_number = (r*(10**p)) + remind_number
         remind_number = c_number // 10
         for i in range(2, c_number//2+1):
@@ -30,4 +31,4 @@ for number in range(499, 2, -2):
     if flag:
         count += 1
         print(result)
-print(count)
+print(f"Count Of Circular Number: {count}")
---
+++ Q-17/README.md
@@ -0,0 +1,5 @@
+<pre>
+    The Number 197, is called a circular prime because all rotations of the digits: (197, 971, 719), are Themselves Prime.
+    There are Thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97
+    How many circular primes are tehre below one milion? 1,000,000
+</pre>
