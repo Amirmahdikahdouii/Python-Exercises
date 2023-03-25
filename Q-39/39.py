89505fc4ef4612a6c60e61e2e34f99f1605c177b
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2023-03-26 01:15:05 +0330

    Updated Q-39

--- Q-39/39.py
+++ Q-39/39.py
@@ -1,7 +1,3 @@
 def passed_numbers(numbers) -> list:
-    passed_nums = []
-    for num in range(min(numbers), max(numbers)+1):
-        if num not in numbers:
-            passed_nums.append(num)
-    return passed_nums
+    return [x for x in range(min(numbers), max(numbers)) if x not in numbers]
 print(passed_numbers([1, 3, 5, 6, 7, 8, 9, 10, 13, 14, 16, 17, 20, 21, 25]))
