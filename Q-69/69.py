fc1bd1f5d13fb90d149efe8842e5a0be1df9fd37
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2023-08-26 15:45:36 +0330

    Q-69/ updated

--- Q-69/69.py
+++ Q-69/69.py
@@ -1,8 +1,16 @@
+from collections import Counter
 # Top-One algorithm
+
+
 def get_top_one(numbers):
-    data = {}
     max_count = 0
     top_ones = []
+    """
+    You can do this instead of creating data:
+    data = Counter(numbers)
+    the Counter object, make a dictionary with key of elements and value is count of elements in iterable
+    """
+    data = {}
     for number in numbers:
         if number in data:
             data[number] += 1
